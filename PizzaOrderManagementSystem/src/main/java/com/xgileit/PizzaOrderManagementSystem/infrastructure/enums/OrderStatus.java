9610b7abefbef374d68534578e52d0cb4dfe2de1
Lee-Rhaan <rhaanking@gmail.com>
2021-11-03 17:32:56 +0200

    Finished documentation on orderStatus enum

--- PizzaOrderManagementSystem/src/main/java/com/xgileit/PizzaOrderManagementSystem/infrastructure/enums/OrderStatus.java
+++ PizzaOrderManagementSystem/src/main/java/com/xgileit/PizzaOrderManagementSystem/infrastructure/enums/OrderStatus.java
@@ -1,15 +1,28 @@
 package com.xgileit.PizzaOrderManagementSystem.infrastructure.enums;
 
+/**
+ * This enum class allows me to set the Status of the Order objects in this Pizza Order System
+ * with predefined enum constants
+ */
+
 public enum OrderStatus {
     PENDING("PENDING"), TRANSFERRED("TRANSFERRED"), PREPARED("PREPARED"), DELIVERED("DELIVERED");
 
     private final String orderStatus;
 
+    /**
+     * Initializing orderStatus variable with argument provided in parameters
+     *
+     * @param orderStatus String
+     */
     OrderStatus(String orderStatus)
     {
         this.orderStatus = orderStatus;
     }
 
+    /**
+     * @return orderStatus of specified object
+     */
     public String getOrderStatus()
     {
         return orderStatus;
