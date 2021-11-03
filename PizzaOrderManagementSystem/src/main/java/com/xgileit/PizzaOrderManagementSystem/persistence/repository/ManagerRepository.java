0368c7b094fa410c335e1bacf999b7f281e77a8e
Lee-Rhaan <rhaanking@gmail.com>
2021-11-03 17:44:50 +0200

    Finished documentation on all repository interfaces

--- PizzaOrderManagementSystem/src/main/java/com/xgileit/PizzaOrderManagementSystem/persistence/repository/ManagerRepository.java
+++ PizzaOrderManagementSystem/src/main/java/com/xgileit/PizzaOrderManagementSystem/persistence/repository/ManagerRepository.java
@@ -5,6 +5,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * I'm extending this interface with "JpaRepository", to get access to all it's CRUD
+ * functionalities.
+ */
 public interface ManagerRepository extends JpaRepository<Manager, Long> {
+
+    /**
+     * This is a custom abstract method I am going to be implementing in my service class.
+     * The name of this method is going to be read like a query by Spring.
+     */
+
+    //setting it to optional, because this method may or may not return a value.
     Optional<Manager> findManagerById(Long id);
 }
--- PizzaOrderManagementSystem/src/main/java/com/xgileit/PizzaOrderManagementSystem/persistence/repository/OrderRepository.java
+++ PizzaOrderManagementSystem/src/main/java/com/xgileit/PizzaOrderManagementSystem/persistence/repository/OrderRepository.java
@@ -5,9 +5,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
+/**
+ * I'm extending this interface with "JpaRepository", to get access to all it's CRUD
+ * functionalities.
+ */
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    /**
+     * These are custom abstract methods I am going to be implementing in my service class.
+     * The names of these methods are going to be read like queries by Spring.
+     */
     void deleteOrderById(Long id);
 
+    //setting it to optional, because this method may or may not return a value.
     Optional<Order> findOrderById(Long id);
 }
